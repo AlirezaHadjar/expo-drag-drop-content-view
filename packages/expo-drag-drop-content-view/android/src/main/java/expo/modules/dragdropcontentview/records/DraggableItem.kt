b1c91f0e43574e60c340df112d1be91950bfdc29
Alireza Hadjar <asiahadjar@gmail.com>
2024-04-06 00:16:46 +0330

    feat(android): added text drag/drop support

---
+++ packages/expo-drag-drop-content-view/android/src/main/java/expo/modules/dragdropcontentview/records/DraggableItem.kt
@@ -0,0 +1,13 @@
+package expo.modules.dragdropcontentview.records
+
+import expo.modules.kotlin.records.Field
+import expo.modules.kotlin.types.Enumerable
+import expo.modules.kotlin.records.Record
+
+enum class DraggableType(val value: String) : Enumerable {
+    IMAGE("image"),
+    VIDEO("video"),
+    TEXT("text"),
+}
+
+data class DraggableItem(@Field val type: DraggableType, @Field val value: String): Record
