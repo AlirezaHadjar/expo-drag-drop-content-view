f578ee34e4e46835a8f067b5b007f5f4811fd5c5
Alireza Hadjar <asiahadjar@gmail.com>
2025-01-13 13:51:12 +0300

    chore: remove ios/android folders

--- packages/expo-drag-drop-content-view/ios/ExpoDragDropContentView.swift
+++ packages/expo-drag-drop-content-view/ios/ExpoDragDropContentView.swift
@@ -8,7 +8,7 @@ class ExpoDragDropContentView: ExpoView {
     let onDragEnd = EventDispatcher()
     let onEnter = EventDispatcher()
     let onExit = EventDispatcher()
-    let dragDropContentView = DragDropContentView() 
+    let dragDropContentView = DragDropContentView()
 
     required init(appContext: AppContext? = nil) {
         super.init(appContext: appContext)
@@ -26,8 +26,35 @@ class ExpoDragDropContentView: ExpoView {
         dragDropContentView.frame = bounds
     }
 
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+       // The event type is incomplete. The number 9 indicates a drag event.
+       // If the drag event has been received, we can direct it to the drag-drop content view.
+       if event?.type.rawValue == 9 {
+         return dragDropContentView.hitTest(point, with: event)
+       }
+
+       // We know that we're not dealing with a drag event.
+       // Other events we want to pass to child views, omitting the `dragDropContentView`.
+       // We don't need to use `reactZIndexSortedSubviews` on Fabric, but it seems to be necessary on Paper.
+       for subview in reactZIndexSortedSubviews() where subview != dragDropContentView {
+         let hit = subview.hitTest(point, with: event)
+         if hit != nil {
+           return hit
+         }
+       }
+
+       // It doesn't seem to be needed, but I'm not 100% sure.
+       return super.hitTest(point, with: event)
+     }
+
     override func addSubview(_ view: UIView) {
         super.addSubview(view)
+
+        handleSubviewAdded(view)
+    }
+
+    override func insertSubview(_ view: UIView, at index: Int) {
+        super.insertSubview(view, at: index)
 
         handleSubviewAdded(view)
     }
